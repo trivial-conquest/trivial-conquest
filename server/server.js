704b003721d956d8296deb0c097ac59ed9c73f9a
charlieperson <charlie.person@hyperisland.se>
2016-11-04 12:13:03 -0500

    Removes comments

--- server/server.js
+++ server/server.js
@@ -14,34 +14,3 @@ app.use('/', router)
 
 const server = app.listen(port)
 console.log(`Server is running on port: ${port}`)
-
-
-// var MongoClient = require('mongodb').MongoClient
-//   , assert = require('assert');
-//
-// // Connection URL
-// var url = 'mongodb://localhost:27017/conquest';
-//
-// // Use connect method to connect to the server
-// MongoClient.connect(url, function(err, db) {
-//   assert.equal(null, err);
-//   console.log("Connected successfully to server");
-//   insertDocuments(db, function() {
-//     db.close();
-//   });
-// });
-//
-// var insertDocuments = function(db, callback) {
-//   // Get the documents collection
-//   var collection = db.collection('documents');
-//   // Insert some documents
-//   collection.insertMany([
-//     {a : 1}, {a : 2}, {a : 3}
-//   ], function(err, result) {
-//     assert.equal(err, null);
-//     assert.equal(3, result.result.n);
-//     assert.equal(3, result.ops.length);
-//     console.log("Inserted 3 documents into the collection");
-//     callback(result);
-//   });
-// }
