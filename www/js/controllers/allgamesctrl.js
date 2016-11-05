6b1083da748cccdaae21f2ae8d6f10838ea76fe3
ppp38 <cmullenx@gmail.com>
2016-11-05 10:04:22 -0500

    remove stateparams

--- www/js/controllers/allgamesctrl.js
+++ www/js/controllers/allgamesctrl.js
@@ -2,6 +2,6 @@ angular.module('trivial.allgames', [])
 
 .controller('AllGamesCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
  //will need to pull all games fom the server and attach them to scope variable
- $stateParams.gameId =1
+ 
   $scope.games= [{name: 'munchybreakfast', id:1}, {name: "settlersof6", id:2}]
 }]);
