8b60b90c58e884e9952bbab87523e9fa8d41f4b9
ppp38 <cmullenx@gmail.com>
2016-11-07 17:40:27 -0600

    fix merge conflict

--- server/fb.js
+++ server/fb.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-        FB_ID: '535661349967737', 
-        FB_SECRET: 'cf696dc89a33b60bb98e2935088fb8e8',
+        FB_ID: '', 
+        FB_SECRET: '',
         TOKEN_SECRET: 'blech'
 }
