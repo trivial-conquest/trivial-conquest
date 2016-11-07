b1e65af355f369cabdac0f5759bdc44474104ab8
ppp38 <cmullenx@gmail.com>
2016-11-07 17:57:37 -0600

    fix problems created by rebase

--- www/js/controllers/allgamesctrl.js
+++ www/js/controllers/allgamesctrl.js
@@ -3,14 +3,16 @@ angular.module('trivial.allgames', [])
 .controller('AllGamesCtrl', ['$scope', '$stateParams', 'gameSrvc', '$window', function($scope, $stateParams, gameSrvc, $window) {
  //will need to pull all games fom the server and attach them to scope variable
 
+ $scope.create = false; 
+ 
  $scope.getGames = function(){
  	  gameSrvc.getAllGames()
  	  .then(function(games){
  	  	console.log('all games retrieved', games)
- 	  	$scope.games = games;
+ 	  	$scope.games = games
  	  })
- 	  .catch(function(err){
- 	  	console.log('no games retrieved', err)
+ 	  .catch(function(){
+ 	  	console.log('no games retrieved')
  	  })
  }
 
@@ -27,15 +29,8 @@ angular.module('trivial.allgames', [])
   	})
   }
 
-// $scope.games= [{name: 'Bens Game', id:'5820e90a30f1ee11c82baf6b', pins:[
-//         {lat: 30.272890, lng: -97.743110},
-//         {lat: 30.267824, lng: -97.745451},
-//         {lat: 30.265921, lng: -97.746274},
-//         {lat: 30.268936, lng: -97.740065}
-//       ]}, {name: "settlersof6", id:2, pins:[
-//         {lat: 30.272890, lng: -97.743110},
-//         {lat: 30.267824, lng: -97.745451}
-//       ]}]
 
 $scope.getGames()
-}]);
+
+   // $scope.games= [{name: 'munchybreakfast', id:1}, {name: "settlersof6", id:2}]
+ }])
