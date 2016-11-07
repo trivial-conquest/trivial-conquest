88610182cc4c64584a6a5d554296141066f8ee40
benjimelito <melito.ben@gmail.com>
2016-11-07 17:01:21 -0600

    Fixed bug real quick

--- www/js/controllers/allgamesctrl.js
+++ www/js/controllers/allgamesctrl.js
@@ -13,11 +13,7 @@ angular.module('trivial.allgames', [])
   	})
   }
 
-
-
-
-   $scope.games= [{name: 'munchybreakfast', id:1}, {name: "settlersof6", id:2}]
- }])  $scope.games= [{name: 'Bens Game', id:'5820e90a30f1ee11c82baf6b', pins:[
+$scope.games= [{name: 'Bens Game', id:'5820e90a30f1ee11c82baf6b', pins:[
         {lat: 30.272890, lng: -97.743110},
         {lat: 30.267824, lng: -97.745451},
         {lat: 30.265921, lng: -97.746274},
