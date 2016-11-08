96463bd42f6ec4df0df0302e14e32dff5440b243
Brett Nelson <brettcnelson@gmail.com>
2016-11-07 19:11:16 -0600

    adds placeholder and fixes formatting

--- www/js/controllers/gamesctrl.js
+++ www/js/controllers/gamesctrl.js
@@ -22,63 +22,63 @@ angular.module('trivial.games', [])
       var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
               // Create the search box and link it to the UI element.
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+      var input = document.getElementById('pac-input');
+      var searchBox = new google.maps.places.SearchBox(input);
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-        // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
+      // Bias the SearchBox results towards current map's viewport.
+      map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+      });
 
-        var markers = [];
+      var markers = [];
         // Listen for the event fired when the user selects a prediction and retrieve
         // more details for that place.
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
+      searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
 
-          if (places.length == 0) {
-            return;
-          }
+        if (places.length == 0) {
+          return;
+        }
 
-          // Clear out the old markers.
-          markers.forEach(function(marker) {
-            marker.setMap(null);
-          });
-          markers = [];
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+          marker.setMap(null);
+        });
+        markers = [];
 
           // For each place, get the icon, name and location.
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            if (!place.geometry) {
-              console.log("Returned place contains no geometry");
-              return;
-            }
-            var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
-            };
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+          if (!place.geometry) {
+            console.log("Returned place contains no geometry");
+            return;
+          }
+          var icon = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+          };
 
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-              map: map,
-              icon: icon,
-              title: place.name,
-              position: place.geometry.location
-            }));
+          // Create a marker for each place.
+          markers.push(new google.maps.Marker({
+            map: map,
+            icon: icon,
+            title: place.name,
+            position: place.geometry.location
+          }));
 
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
+          if (place.geometry.viewport) {
+            // Only geocodes have viewport.
+            bounds.union(place.geometry.viewport);
+          } else {
+            bounds.extend(place.geometry.location);
+          }
         });
+          map.fitBounds(bounds);
+      });
 
       var youMarker = new google.maps.Marker({
         position: latLng,
@@ -110,28 +110,6 @@ angular.module('trivial.games', [])
         });
         map.panTo(location);  
       }
-
-     
-
-      var drawingManager = new google.maps.drawing.DrawingManager({
-        drawingMode: google.maps.drawing.OverlayType.MARKER,
-        drawingControl: true,
-        drawingControlOptions: {
-          position: google.maps.ControlPosition.TOP_CENTER,
-          drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
-        },
-        markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
-        circleOptions: {
-          fillColor: '#ffff00',
-          fillOpacity: 1,
-          strokeWeight: 5,
-          clickable: false,
-          editable: true,
-          zIndex: 1
-        }
-      });
-
-      drawingManager.setMap(map);
 
       var pins = $scope.game.pins
 
@@ -184,81 +162,31 @@ angular.module('trivial.games', [])
 
       drop()
 
-
     })
+
     .catch(function(error){
     console.log("Could not get location", error);
-    });
+  });
 
 }]);
 
+// THIS IS THE OLD DRAWING LIBRARY 
+//  var drawingManager = new google.maps.drawing.DrawingManager({
+//   drawingMode: google.maps.drawing.OverlayType.MARKER,
+//   drawingControl: true,
+//   drawingControlOptions: {
+//     position: google.maps.ControlPosition.TOP_CENTER,
+//     drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
+//   },
+//   markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
+//   circleOptions: {
+//     fillColor: '#ffff00',
+//     fillOpacity: 1,
+//     strokeWeight: 5,
+//     clickable: false,
+//     editable: true,
+//     zIndex: 1
+//   }
+// });
 
-      // function initAutocomplete() {
-      //   var map = new google.maps.Map(document.getElementById('map'), {
-      //     center: {lat: -33.8688, lng: 151.2195},
-      //     zoom: 13,
-      //     mapTypeId: 'roadmap'
-      //   });
-
-        // // Create the search box and link it to the UI element.
-        // var input = document.getElementById('pac-input');
-        // var searchBox = new google.maps.places.SearchBox(input);
-        // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-        // // Bias the SearchBox results towards current map's viewport.
-        // map.addListener('bounds_changed', function() {
-        //   searchBox.setBounds(map.getBounds());
-        // });
-
-        // var markers = [];
-        // // Listen for the event fired when the user selects a prediction and retrieve
-        // // more details for that place.
-        // searchBox.addListener('places_changed', function() {
-        //   var places = searchBox.getPlaces();
-
-        //   if (places.length == 0) {
-        //     return;
-        //   }
-
-        //   // Clear out the old markers.
-        //   markers.forEach(function(marker) {
-        //     marker.setMap(null);
-        //   });
-        //   markers = [];
-
-        //   // For each place, get the icon, name and location.
-        //   var bounds = new google.maps.LatLngBounds();
-        //   places.forEach(function(place) {
-        //     if (!place.geometry) {
-        //       console.log("Returned place contains no geometry");
-        //       return;
-        //     }
-        //     var icon = {
-        //       url: place.icon,
-        //       size: new google.maps.Size(71, 71),
-        //       origin: new google.maps.Point(0, 0),
-        //       anchor: new google.maps.Point(17, 34),
-        //       scaledSize: new google.maps.Size(25, 25)
-        //     };
-
-        //     // Create a marker for each place.
-        //     markers.push(new google.maps.Marker({
-        //       map: map,
-        //       icon: icon,
-        //       title: place.name,
-        //       position: place.geometry.location
-        //     }));
-
-        //     if (place.geometry.viewport) {
-        //       // Only geocodes have viewport.
-        //       bounds.union(place.geometry.viewport);
-        //     } else {
-        //       bounds.extend(place.geometry.location);
-        //     }
-        //   });
-        //   map.fitBounds(bounds);
-        // });
-      // }
-
-
-
+// drawingManager.setMap(map);
