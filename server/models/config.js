63785825c76eb055b0d6461332c2ba01d6f65ecf
charlieperson <charlie.person@hyperisland.se>
2016-11-09 15:44:45 -0600

    Passing two tests

--- server/models/config.js
+++ server/models/config.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise
 
-const db = mongoose.connect('mongodb://localhost:27017/conquest');
+var mongoURI = {
+  development: 'mongodb://localhost:27017/conquest',
+  test: 'mongodb://localhost:27017/conquest-test'
+};
+
+const db = mongoose.connect(mongoURI[process.env.NODE_ENV], function(err, res) {
+  if(err) {
+    console.log('Error connecting to the database. ' + err);
+  } else {
+    console.log('Connected to Database: ' + mongoURI[process.env.NODE_ENV]);
+  }
+});
+
 module.exports = db;
