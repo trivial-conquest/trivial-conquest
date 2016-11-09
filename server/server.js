5066f4c01f604f722c8019e5f904b15f4d7f7f8c
ppp38 <cmullenx@gmail.com>
2016-11-08 18:12:40 -0600

    fix post 401 error

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ function isAuthenticated(req, res, next) {
 
 
 app.use('/', router)
-app.use('/games', games)
+app.use('/games', isAuthenticated, games)
 app.use('/auth', auth)
 
 
--- www/js/app.js
+++ www/js/app.js
@@ -30,11 +30,13 @@ angular.module('trivial', ['ionic', 'trivial.login', 'trivial.map', 'trivial.gam
   };
 
   if (ionic.Platform.isIOS() || ionic.Platform.isAndroid()) {
-  commonConfig.redirectUri = 'http://localhost:8080/';
+    commonConfig.redirectUri = 'http://localhost:8080/';
+    // commonConfig.redirectUri = 'https://intense-journey-25938.herokuapp.com/auth/facebook';
   }
   $authProvider.facebook(angular.extend({}, commonConfig, {
-  clientId: '',
+  clientId: '535661349967737',
   url: 'http://localhost:8080/auth/facebook'
+  // url: 'https://intense-journey-25938.herokuapp.com/auth/facebook'
   }));
 
 
