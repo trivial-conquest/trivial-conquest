d8624519bf726df95d7a7d1e10a92ea7ccd7ce6d
ppp38 <cmullenx@gmail.com>
2016-11-08 18:05:19 -0600

    add babel to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,15 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var babel = require("gulp-babel");
+var plumber = require("gulp-plumber");
 
 var paths = {
+  es6: ['./src/es6/*.js'],
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['babel','sass']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -26,7 +29,16 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
+gulp.task("babel", function () {
+  return gulp.src(paths.es6)
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(gulp.dest("www/js"));
+});
+
+
 gulp.task('watch', function() {
+  gulp.watch(paths.es6, ['babel']);
   gulp.watch(paths.sass, ['sass']);
 });
 
