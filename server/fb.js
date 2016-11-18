54a7dbed769aacc3a3f95651da46c56013f9db0e
ppp38 <cmullenx@gmail.com>
2016-11-17 18:52:23 -0600

    more frontend tests, all frontend tests passing

--- server/fb.js
+++ server/fb.js
@@ -1,11 +1,6 @@
 
 module.exports = {
-<<<<<<< 1d2f40b3f9d885c9576bf48fdf6a5493cbc8f585
-        FB_ID: '1789849501229484',
-        FB_SECRET: 'ca8fdd38589256173d1cd9b56a81740c',
-=======
         FB_ID: '535661349967737',
         FB_SECRET: 'cf696dc89a33b60bb98e2935088fb8e8',
->>>>>>> change token expiration
-        TOKEN_SECRET: 'blech'
+       TOKEN_SECRET: 'blech'
 };
