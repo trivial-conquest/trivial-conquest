5e2721e0ebf7edf9f4ea61e66fe8e1047950fdd9
ppp38 <cmullenx@gmail.com>
2016-11-17 18:52:23 -0600

    more frontend tests, all frontend tests passing

--- server/fb.js
+++ server/fb.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-FB_ID: '535661349967737',
-FB_SECRET: 'cf696dc89a33b60bb98e2935088fb8e8',
-TOKEN_SECRET: 'blech'
+	FB_ID: '535661349967737',
+	FB_SECRET: 'cf696dc89a33b60bb98e2935088fb8e8',
+	TOKEN_SECRET: 'blech'
 };
