f2ba941fe228f02150ce779de599b7368b60f208
ppp38 <cmullenx@gmail.com>
2016-11-21 18:54:50 -0600

    change allgames to remove show from buttons

--- www/js/controllers/gamesctrl.js
+++ www/js/controllers/gamesctrl.js
@@ -236,7 +236,7 @@ angular.module('trivial.games', [])
             var userPoints = gameRes[0].points
             var outcome = (Math.random() * (pinPoints + userPoints))
             // console.log("CLOSEST", closest)
-            alert('You are attacking ' + closest.name + '. It is worth ' + closest.points + ' points and you have a ' + (userPoints/(pinPoints + userPoints)) * 100 + '% chance of winning!')
+            SweetAlert.swal('You are attacking ' + closest.name + '. It is worth ' + closest.points + ' points and you have a ' + (userPoints/(pinPoints + userPoints)) * 100 + '% chance of winning!')
             if(outcome < userPoints) {
              SweetAlert.swal('Victory is yours!')
               //Takes a winner first and then a loser, so in this case the user wins
